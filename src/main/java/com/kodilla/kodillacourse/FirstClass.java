1a9ea7db23b4f5d39a99dd2a4c0c8d742949c523
J-Fi <y.neczek@gmail.com>
2019-05-08 00:26:03 +0200

    Module 6.1

--- src/main/java/com/kodilla/kodillacourse/FirstClass.java
+++ src/main/java/com/kodilla/kodillacourse/FirstClass.java
@@ -1,4 +1,53 @@
 package com.kodilla.kodillacourse;
 
+interface Knight {
+    String fight();
+}
+
+class KnightOne implements Knight {
+    String name;
+
+    public KnightOne(String name) {
+        this.name = name;
+    }
+
+    public String fight() {
+        return this.name + " Bitwa pod Grunwaldem";
+    }
+
+}
+
+class KnightTwo implements Knight {
+    String name;
+
+    public KnightTwo(String name) {
+        this.name = name;
+    }
+
+    public String fight() {
+        return this.name + ": Bitwa pod Koronowem";
+    }
+
+}
+
 public class FirstClass {
+
+    public static void main(String[] args) {
+
+        Knight knight = new KnightTwo("Konrad");
+        FirstClass.info(knight);
+
+        if ((knight instanceof Knight)) {
+            System.out.println("\nTak, ten obiekt jest instancją interfejsu Knight");
+        }
+
+        System.out.println(("OK").hashCode());
+        System.out.println(("OK1").hashCode());
+        System.out.println(("OK2").hashCode());
+    }
+
+    public static void info(Knight knight) {
+        System.out.print(knight.fight());
+    }
+
 }
