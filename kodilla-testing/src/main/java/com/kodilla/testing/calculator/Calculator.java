209ac428a9605fbfec63b72ef89d313f7bfe7a9a
J-Fi <y.neczek@gmail.com>
2019-05-08 23:44:34 +0200

    Caculator update 2

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,8 +1,8 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    int a;
-    int b;
+    private int a;
+    private int b;
 
     public Calculator (int a, int b) {
         this.a = a;
