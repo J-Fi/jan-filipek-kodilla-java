be729d54f0c47a5e3c44e559bae7ccdc6a7740ff
J-Fi <y.neczek@gmail.com>
2019-08-02 00:42:13 +0200

    17.4 excercise NamedQueries

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -6,6 +6,12 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@NamedNativeQuery(
+        name = "Company.retrieveCompanyNamesWhichFirstThreeLettersAre",
+        query = "SELECT * FROM COMPANIES WHERE SUBSTR(COMPANY_NAME , 1, 3) = :FIRSTTHREELETTERS",
+        resultClass = Company.class
+)
+
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -9,11 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+
+    @Autowired
+    EmployeeDao employeeDao;
 
     @Test
     public void testSaveManyToMany(){
@@ -46,18 +51,23 @@ public class CompanyDaoTestSuite {
         companyDao.save(greyMatter);
         int greyMatterId = greyMatter.getId();
 
+        List<Company> companyNamesWhichFirstThreeLettersAre = companyDao.retrieveCompanyNamesWhichFirstThreeLettersAre("Sof");
+        List<Employee> employeeSpecificNameOf = employeeDao.retrieveEmployeeSpecificNameOf("Smith");
+
         //Then
         Assert.assertNotEquals(0, softwareMachineId);
         Assert.assertNotEquals(0, dataMaestersId);
         Assert.assertNotEquals(0, greyMatterId);
+        Assert.assertEquals(1, companyNamesWhichFirstThreeLettersAre.size());
+        Assert.assertEquals(1, employeeSpecificNameOf.size());
 
         //CleanUp
-        /*try {
+        try {
             companyDao.deleteById(softwareMachineId);
             companyDao.deleteById(dataMaestersId);
             companyDao.deleteById(greyMatterId);
         } catch (Exception e) {
             //do nothing
-        }*/
+        }
     }
 }
