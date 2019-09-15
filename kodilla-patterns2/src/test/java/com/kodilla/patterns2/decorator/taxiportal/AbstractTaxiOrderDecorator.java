6bafaf5682fd23b189150f9a5874954514feabf8
J-Fi <y.neczek@gmail.com>
2019-09-15 14:32:26 +0200

    MOD24.2: add all required decorator classes

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/taxiportal/AbstractTaxiOrderDecorator.java
+++
@@ -1,4 +0,0 @@
-package com.kodilla.patterns2.decorator.taxiportal;
-
-public class AbstractTaxiOrderDecorator {
-}
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/taxiportal/TaxiOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/taxiportal/TaxiOrderTestSuite.java
@@ -26,4 +26,54 @@ public class TaxiOrderTestSuite {
         //Then
         assertEquals("Drive a course", description);
     }
+
+    @Test
+    public void testTaxiNetworkGetCost() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        theOrder = new TaxiNetworkOrderDecorator(theOrder);
+        //When
+        BigDecimal theCost = theOrder.getCost();
+        //Then
+        assertEquals(new BigDecimal(40), theCost);
+    }
+
+    @Test
+    public void testTaxiNetworkGetDescription() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        theOrder = new TaxiNetworkOrderDecorator(theOrder);
+        //When
+        String description = theOrder.getDescription();
+        //Then
+        assertEquals("Drive a course by Taxi Network", description);
+    }
+
+    @Test
+    public void testMyTaxiWithChildSeatsGetCost() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        theOrder = new MyTaxiNetworkOrderDecorator(theOrder);
+        theOrder = new ChildSeatDecorator(theOrder);
+        theOrder = new ExpressDecorator(theOrder);
+        theOrder = new VipDecorator(theOrder);
+        //When
+        BigDecimal cost = theOrder.getCost();
+        //Then
+        assertEquals(new BigDecimal(322), cost);
+    }
+
+    @Test
+    public void testMyTaxiWithChildSeatsGetDescription() {
+        //Given
+        TaxiOrder theOrder = new BasicTaxiOrder();
+        theOrder = new MyTaxiNetworkOrderDecorator(theOrder);
+        theOrder = new ChildSeatDecorator(theOrder);
+        theOrder = new ExpressDecorator(theOrder);
+        theOrder = new VipDecorator(theOrder);
+        //When
+        String description = theOrder.getDescription();
+        //Then
+        assertEquals("Drive a course by MyTaxi Network + child seat + express service + VIP service", description);
+    }
 }
